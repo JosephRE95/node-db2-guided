c9ba9b61f2c91f1e149163a6420ce397fafca2af
Joseph Ellis <josephellis95@gmail.com>
2024-04-24 15:16:38 -0700

    wip

--- data/migrations/20240424210606_first-migration.js
+++ data/migrations/20240424210606_first-migration.js
@@ -4,7 +4,10 @@
  */
 exports.up = async function(knex) {
   await knex.schema.createTable('fruits', table =>{
-    table.increments('fruits_id')
+    table.increments('fruit_id')
+    table.string('fruit_name', 20).notNullable().unique()
+    table.decimal('ave_weight_oz').notNullable()
+    table.boolean('dellish')
   })
 };
 
